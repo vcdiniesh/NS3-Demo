ea99ca52ee2edf36bb3c9cf62e129fd9e6c566f4
vcdiniesh <40363280+vcdiniesh@users.noreply.github.com>
2018-08-15 21:28:14 +0530

    Add files via upload

---
+++ first-PCAP.cc
@@ -0,0 +1,71 @@
+// ****************************************************************************************
+//Self Organizing Networking Group (SONG) @ Kongu Engineering College (KEC) 
+ 
+// ****************************************************************************************
+/*
+ This example program helps to enable PCAP Module 
+ By default, the program not have suppport with PCAP the following modification hels to enable packet capture.
+STEP1: I hope that based on NS3 Installation procedure Wireshark is installed
+STEP2: Add EnablePcapAll ("file_name"); at last before Simulator::Run (); 
+STEP3: ./waf --run scratch/first-PCAP
+	and file_name.pcap file will be generated and open in wireshark*/
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("FirstScriptExample");
+
+int
+main (int argc, char *argv[])
+{
+  CommandLine cmd;
+  cmd.Parse (argc, argv);
+  
+  Time::SetResolution (Time::NS);
+  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+  LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+
+  NodeContainer nodes;
+  nodes.Create (2);
+
+  PointToPointHelper pointToPoint;
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+
+  NetDeviceContainer devices;
+  devices = pointToPoint.Install (nodes);
+
+  InternetStackHelper stack;
+  stack.Install (nodes);
+
+  Ipv4AddressHelper address;
+  address.SetBase ("10.1.1.0", "255.255.255.0");
+
+  Ipv4InterfaceContainer interfaces = address.Assign (devices);
+
+  UdpEchoServerHelper echoServer (9);
+
+  ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
+  serverApps.Start (Seconds (1.0));
+  serverApps.Stop (Seconds (10.0));
+
+  UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);
+  echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
+  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+
+  ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
+  clientApps.Start (Seconds (2.0));
+  clientApps.Stop (Seconds (10.0));
+//Enable PCAP
+  pointToPoint.EnablePcapAll ("myfirst");
+
+  Simulator::Run ();
+  Simulator::Destroy ();
+  return 0;
+}
